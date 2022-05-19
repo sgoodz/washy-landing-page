1d7b05dfb7a52be2d5aa547d024d3dbde6d75e22
saulGoodz <saulgooding@dunsterhouse.co.uk>
2022-05-19 10:42:40 +0100

    initial commit - Vite/Vue3/Tailwind

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1 class="text-6xl font-bold">Vue 3 + Vite + Tailwindcss</h1>
+</template>
+
+<style>
+#app {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
